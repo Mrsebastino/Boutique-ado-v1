d3787d2b1dc69bd878bfc9a2be82edc8c179c6c5
Sebastien Denommee <mrsebastino@gmail.com>
2020-07-14 12:01:41 +0000

    created checkout apps and models

--- bag/views.py
+++ bag/views.py
@@ -35,8 +35,8 @@ def add_to_bag(request, item_id):
                     request, f'Added size {size.upper()} {product.name} to your bag')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(
-                request, f'Added size {size.upper()} {product.name} to your bag')
+            messages.success(request,
+                             f'Added size {size.upper()} {product.name} to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
