a9fcc9c3dabf538ea084b30a0de095c2d7a68a8b
Sebastien Denommee <mrsebastino@gmail.com>
2020-07-17 11:49:12 +0000

    added success checkout logic

--- checkout/models.py
+++ checkout/models.py
@@ -28,7 +28,7 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
 
-    def _generate_order_numbert(self):
+    def _generate_order_number(self):
         """
         Generate a random, unique number
         """
@@ -39,8 +39,7 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))[
-            'lineitem_total_sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * \
                 settings.STANDARD_DELIVERY_PERCENTAGE / 100
