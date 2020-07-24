47aa70b8f78934110e333e2e58fdea253c920dea
Sebastien Denommee <mrsebastino@gmail.com>
2020-07-24 13:57:04 +0000

    product admin editing products

--- products/views.py
+++ products/views.py
@@ -8,6 +8,7 @@ from .forms import ProductForm
 
 # Create view here
 
+
 def all_products(request):
     """A view to show all products, including sorting and search"""
     products = Product.objects.all()
@@ -74,19 +75,45 @@ def product_detail(request, product_id):
 def add_product(request):
     """ Add product to the store """
     if request.method == "POST":
-       form = ProductForm(request.POST, request.FILES)
-       if form.is_valid():
-          form.save()
-          messages.success(request, 'Successfully added product!')
-          return redirect(reverse('add_product'))
-       else:
-           messages.error(request, 'Failed to add product. PLease ensure the form is valid.')
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(
+                request, 'Failed to add product. PLease ensure the form is valid.')
     else:
-        form = ProductForm()     
-    
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request, 'Failed to update product. PLease ensure form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing { product.name }')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
