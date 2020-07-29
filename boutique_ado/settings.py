334f0855cfe1df7038545281b0ebda894ec363fc
Sebastien Denommee <mrsebastino@gmail.com>
2020-07-29 19:42:57 +0000

    deploy with aws

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,6 +178,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'files'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_S3_OBJECT_PARAMETERS = {
