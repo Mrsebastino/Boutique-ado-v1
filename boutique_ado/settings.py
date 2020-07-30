8f1b0faf8a5c3ea69ee962bfaf4b2cbaa4a6945e
Sebastien Denommee <mrsebastino@gmail.com>
2020-07-30 10:56:42 +0000

    deploy to heroku with cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,14 +178,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
-# if 'USE_AWS' in os.environ:
-if True:
-    # Bucket Configs
+if 'USE_AWS' in os.environ:
+    # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
+    # Bucket Configs
     AWS_STORAGE_BUCKET_NAME = 'sebastien-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
