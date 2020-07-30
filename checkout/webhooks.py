a3419230a9287dbfb1e32ebcaf1a5ffd9a831d8c
Sebastien Denommee <mrsebastino@gmail.com>
2020-07-30 12:25:03 +0000

    testing webhook with heroku

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -11,13 +11,12 @@ import stripe
 @require_POST
 @csrf_exempt
 def webhook(request):
-    """ listen for webhooks from stripe """
-
+    """Listen for webhooks from Stripe"""
     # Setup
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    # Get the webhook data and verify it
+    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
@@ -28,30 +27,32 @@ def webhook(request):
         )
     except ValueError as e:
         # Invalid payload
+        print('error1')
         return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
+        print('error2')
         return HttpResponse(status=400)
     except Exception as e:
+        print('error3')
         return HttpResponse(content=e, status=400)
 
-    # set up a webhook handler
+    # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
-    # map webhook events to relevant handler functions
+    # Map webhook events to relevant handler functions
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed':
-        handler.handle_payment_intent_payment_failed,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
     }
 
-    # Get the webhook from stripe
+    # Get the webhook type from Stripe
     event_type = event['type']
 
-    # if there is a handler for it, get it from the event map
-    # use the generic one by default
+    # If there's a handler for it, get it from the event map
+    # Use the generic one by default
     event_handler = event_map.get(event_type, handler.handle_event)
 
-    # call the event handler with the event
+    # Call the event handler with the event
     response = event_handler(event)
     return response
